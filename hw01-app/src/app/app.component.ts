bcb2adfc057d7719f9a916716b7a553f6ba5dad1
sergeybiophys <roven2hati@gmail.com>
2021-11-20 19:09:06 +0200

    added components: passport-info&person-info and interfaces

--- hw01-app/src/app/app.component.ts
+++ hw01-app/src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
 
+export interface PassportInterface {
+  series: string;
+  number: string;
+}
+export interface PersonInterface {
+  firstName: string;
+  lastName: string;
+  middleName: string;
+  birth: Date;
+  profession: string;
+  passport: PassportInterface;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'hw01-app';
+  title: string = 'hw01-app';
+
+  passport: PassportInterface = {
+    series: 'HH',
+    number: '000001'
+  };
+
+  person: PersonInterface = {
+    firstName: 'Somebody',
+    lastName: 'Unknown',
+    middleName:'New',
+    birth: new Date('01-01-2001'),
+    profession: 'super',
+    passport: this.passport
+
+  }
 }
+
